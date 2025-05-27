d4b95afa458f1443cc05cdadce13e0a1aaffbc16
Mauricio Salas Hernández <A01710309@tec.mx>
2025-05-26 18:56:08 -0600

    about us

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -6,17 +6,17 @@ const AboutUs = () => {
       <div className="flex flex-col md:flex-row items-center justify-center">
         <div className="md:w-1/2">
           <h2 className="lg:max-w-[40vw] font-bold text-2xl md:text-3xl text-left">
-            ¿Quién es <span className="text-purple">Mauricio Salas?</span>
+            Nuestra <span className="text-purple">visión</span>
           </h2>
           <p className="text-white-200 md:mt-5 my-5 text-justify">
-            Soy un estudiante mexicano de 21 años que disfruta pasar tiempo de
-            calidad con su familia y amigos, conectar con la naturaleza y
-            aprender. Me apasiona la calistenia y la escalada,{" "}
+            En Sorelli creamos sitios que proyectan confianza, comunican con
+            claridad y están hechos para convertir.
             <span className="font-extralight">
-              además de tener un gran interés por el emprendimiento.{" "}
+              Unimos diseño visual y desarrollo moderno para que tu negocio
+              destaque con una presencia digital que realmente lo represente.{" "}
             </span>
-            Actualmente, estoy enfocado en combinar mi carrera como ingeniero en
-            software con mi pasión por el marketing y los negocios.
+            Cada proyecto está diseñado a la medida. Nada genérico. Solo
+            soluciones que conectan, venden y perduran.
           </p>
         </div>
         <div className="md:w-1/2 flex justify-center ">
